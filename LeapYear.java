a6f276f5f69db5c1f1ea2c2c50f3ad43bec21335
NehaDSingh <113004554+NehaDSingh@users.noreply.github.com>
2022-09-07 12:23:55 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,18 @@
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		
+		int year= 2032;
+		
+		if (year % 4 == 0)
+		{
+			System.out.println("Leap Year is "+ year);
+		}
+		else
+		{
+        System.out.println("Leap Year is not "+ year);
+        }
+	
+	}
+}
