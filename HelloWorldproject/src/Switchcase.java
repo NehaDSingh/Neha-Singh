112a13bf9f11ad03b0b7f956ac8725cbe4718db6
abhishke singh <abhishke singh@LAPTOP-6HPFDKIK.mshome.net>
2022-09-07 11:47:55 +0530

    first commit

---
+++ HelloWorldproject/src/Switchcase.java
@@ -0,0 +1,17 @@
+
+public class Switchcase {
+
+	public static void main(String[] args) {
+		 
+		  char c='g';
+		  
+		  switch(c)
+		  {
+		  case 'a','e','i','o','u'->System.out.println("vowel");
+		  
+		  default->System.out.println("consonant");
+		  }
+
+	}
+
+}
---
+++ HelloWorldproject/src/newswitchcase.java
@@ -0,0 +1,25 @@
+
+public class newswitchcase {
+
+	public static void main(String[] args) {
+      
+		String month="Feb";
+		int year=2024;
+		
+		switch(month)
+		{
+		case "Jan","March","May","July","Aug","Oct","Dec"->System.out.println("Number of the days=31");
+		case "April","June","Sep","Nov"->System.out.println("Number of the days=30");
+		case "Feb"->{
+			          if(year%4==0)
+			        	  System.out.println("Leap Year and Number of days=29");
+			          else
+			        	  System.out.println("Number of days=28");
+		}
+		default->System.out.println("Invalid month");
+		           
+		}
+
+	}
+
+}
